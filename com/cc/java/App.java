68b61c2312f7d48b51f0630906ceadd176afa4f7
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-30 12:47:20 +0200

    // Besser: Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -16,15 +16,14 @@ public class App {
         output("------  Differenzen ------");
         output("Diff. r-t: " + areaDiff(rectangle, triangle));
         output("Diff. r-c: " + areaDiff(rectangle, circle ));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
+
     }
     
-    // 1. Variante ggf. 1000x überladen........... 
-    private static double areaDiff(Rectangle r, Triangle t) {
-        return r.area() - t.area();
-    }
-
-    private static double areaDiff(Rectangle r, Circle c) {
-        return r.area() - c.area();
+    // Besser: Polymorphie
+    private static double areaDiff(Shape s1, Shape s2) {
+        return s1.area() - s2.area();
     }
 
     private static void output(String outputStr) {
